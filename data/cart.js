a9f1e5986993d2b7b4cf434c7890fcbf8712a3f8
ArachnoStarkCodes <ArachnoStarkCodes@users.noreply.github.com>
2024-07-09 16:07:55 +0530

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,33 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+  [
+      {
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+      },
+      {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+      }
+  ];
+  // console.log(cart);
 
-if (!cart){
-    cart = [
-        {
-            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            quantity: 2
-        },
-        {
-            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity: 1
-        }
-    ];
-    console.log(cart);
-}
+// if (!cart){
+//     cart = [
+//         {
+//             productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+//             quantity: 2,
+//             deliveryOptionId: '1'
+//         },
+//         {
+//             productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+//             quantity: 1,
+//             deliveryOptionId: '2'
+//         }
+//     ];
+//     console.log(cart);
+// }
 
 // export let cart = [];
 
@@ -35,7 +50,8 @@ export function addToCart(productId, quantity){
     } else {        
         cart.push({
         productId,
-        quantity
+        quantity,
+        deliveryOptionId: '1'
       });
     }
 
@@ -59,6 +75,8 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+
+    // console.log(cart.length);
 
     saveToStorage();
 
