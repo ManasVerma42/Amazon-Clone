e63a32003241079e9bac9781ae201848ac623f88
ArachnoStarkCodes <manas.sudip.verma9426@gmail.com>
2024-07-13 23:30:06 +0530

    convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,19 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 import {loadCart} from "../data/cart.js";
 // import '../data/backend-practice.js';
 // import '../data/cart-class.js';
 
 //Loads Multiple promises at the same time
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve();
-            // resolve('value1');
-        });
-    }),
+    // new Promise((resolve) => {
+    //     loadProducts(() => {
+    //         resolve();
+    //         // resolve('value1');
+    //     });
+    // }),
+    loadProductsFetch(),
     new Promise(resolve => {
         loadCart(() => {
             resolve();
